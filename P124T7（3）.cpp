b913d1f82b9d028e9d5a3016a3ba7cd6e845ae7a
0Weerus0 <107356075+0Weerus0@users.noreply.github.com>
2022-10-09 15:35:48 +0800

    Add files via upload

---
+++ P124T7（3）.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Student
+{public:
+	Student (int n, float s):num(n),score(s){}
+	void change(int n,float s){num=n;score=s;}
+	void display(){cout<<num<<" "<<score<<endl;}
+ private:
+ 	int num;
+	 float score;};
+int main(){
+	Student stud(101,78.5);
+	const Student *p=&stud;//(3)新建指针来实现调用 
+	p->display();
+	p->change(101,80.5);
+	p->display();
+	return 0;
+}
