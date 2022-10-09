4b1353a77026867d687d26dcd6e8e02dec7d0051
Kagami <zhixiaer@outlook.com>
2022-10-09 15:48:23 +0800

    原始代码
    让你传个没改const的原始代码都这么费劲是吧

--- P124T7.cpp
+++ P124T7.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
 using namespace std; 
 class Student
-{public:
-	Student (int n, float s):num(n),score(s){}
-	void change(int n,float s)const{
-		const int *p=&num;
-		*p=&n;
-		
-	}
-	void display()const{cout<<num<<" "<<score<<endl;}
- private:
- 	int num;
-	 float score;};
+{
+	public:
+		Student (int n, float s):num(n),score(s){}
+		void change(int n,float s){ num=n; score=s;}
+		void display(){cout<<num<<" "<<score<<endl;}
+ 	private:
+ 		int num;
+		float score;
+};
+
 int main(){
-	const Student stud(101,78.5);//（1）const对象无法被外部引用 
+	Student stud(101,78.5);
 	stud.display();
 	stud.change(101,80.5);
 	stud.display();
