79eed1643e547fc3ed7505473804e5cc29eee87d
Kagami <zhixiaer@outlook.com>
2022-10-09 15:59:29 +0800

    改为常对象
    将stud声明为常对象并因此部分修改了Student类定义

--- P124T7.cpp
+++ P124T7.cpp
@@ -4,15 +4,20 @@ class Student
 {
 	public:
 		Student (int n, float s):num(n),score(s){}
-		void change(int n,float s){ num=n; score=s;}
-		void display(){cout<<num<<" "<<score<<endl;}
+		void change(int n,float s)const{
+			int* int_p = (int*)&num;
+			*int_p = n;
+			float* float_p = (float*)&score;
+			*float_p = s;
+		}
+		void display()const{cout<<num<<" "<<score<<endl;}
  	private:
  		int num;
 		float score;
 };
 
 int main(){
-	Student stud(101,78.5);
+	const Student stud(101,78.5);
 	stud.display();
 	stud.change(101,80.5);
 	stud.display();
